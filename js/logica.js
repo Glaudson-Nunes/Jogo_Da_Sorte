caa06917c511946c5d2c1c1953f18420ea7019bd
GLAUDSON NUNES <93484378+Glaudson-Nunes@users.noreply.github.com>
2023-12-03 21:55:37 -0300

    Add files via upload

---
+++ js/logica.js
@@ -0,0 +1,230 @@
+
+
+let baus = [
+    {
+      id: 1,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '10',
+      premio: "Parabéns! Você Ganhou R$0,50",
+    },
+    {
+      id: 2,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '5',
+      premio: "Parabéns! Você Ganhou R$ 00,20!",
+    },
+    {
+      id: 3,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '15',
+      premio: "Parabéns! Você Ganhou R$ 2,00",
+    },
+    {
+      id: 4,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '8',
+      premio: "Parabéns! Você Ganhou R$ 10,00",
+    },
+    {
+      id: 5,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '12',
+      premio: "Parabéns! Você Ganhou R$ 5,00",
+    },
+    {
+      id: 6,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '25',
+      premio: "Parabéns! Você Ganhou R$ 3,00",
+    },
+    {
+      id: 7,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '18',
+      premio: "Parabéns! Você Ganhou R$ 00,20",
+    },
+    {
+      id: 8,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '3',
+      premio: "Parabéns! Você Ganhou R$ 20,00",
+    },
+    {
+      id: 9,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '7',
+      premio: "Parabéns! Você Ganhou R$ 5,00",
+    },
+    {
+      id: 10,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '14',
+      premio: "Parabéns! Você Ganhou R$ 3,00",
+    },
+    {
+      id: 11,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '30',
+      premio: "Parabéns! Você Ganhou R$ 5,00",
+    },
+    {
+      id: 12,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '9',
+      premio: "Parabéns! Você Ganhou R$ 00,50",
+    },
+    {
+      id: 13,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '20',
+      premio: "Parabéns! Você Ganhou R$ 10,00",
+    },
+    {
+      id: 14,
+      foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+      foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+      numero: '11',
+      premio: "Parabéns! Você Ganhou R$ 00,20",
+    },
+
+    {
+        id: 15,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '16',
+        premio: "Parabéns! Você Ganhou R$ 00,40",
+    },
+    {
+        id: 16,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '22',
+        premio: "Parabéns! Você Ganhou R$ 5,00",
+    },
+    {
+        id: 17,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '27',
+        premio: "Parabéns! Você Ganhou R$ 10,00",
+    },
+    {
+        id: 18,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '13',
+        premio: "Parabéns! Você Ganhou R$ 20,00",
+    },
+    {
+        id: 19,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '35',
+        premio: "Parabéns! Você Ganhou R$ 50,00",
+    },
+    {
+        id: 20,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '19',
+        premio: "Parabéns! Você Ganhou R$ 00,30",
+    },
+
+    {
+        id: 21,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '24',
+        premio: "Parabéns! Você Ganhou R$ 100,00",
+    },
+    {
+        id: 22,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '33',
+        premio: "Parabéns! Você Ganhou R$ 00,25",
+    },
+    {
+        id: 23,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '28',
+        premio: "Parabéns! Você Ganhou R$ 15,00",
+    },
+    {
+        id: 24,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '36',
+        premio: "Parabéns! Você Ganhou R$ 50,00",
+    },
+    {
+        id: 25,
+        foto1: "https://img.icons8.com/?size=48&id=gWjmMVhnilA9&format=gif",
+        foto2: "https://img.icons8.com/?size=48&id=g7FGqx8h2AxR&format=gif",
+        numero: '23',
+        premio: "Parabéns! Você Ganhou R$ 00,10",
+    },
+
+    
+  ];
+
+
+  let box = document.querySelector(".box");
+  let modal = document.querySelector('.modal');
+  let textPremio = document.querySelector("#textPremio");
+  let fecharModal = document.querySelector('#fecharModal').addEventListener("click", (()=>{
+
+    modal.style.display = 'none'
+    
+  }))
+
+  let btreiniciar = document.querySelector('#btreiniciar');
+  btreiniciar.addEventListener("click", ()=> reiniciarGame());
+ 
+  function ListarBaus(){
+
+    baus.forEach((bau)=>{
+
+        const img = document.createElement('img');
+        img.classList.add('imgFechada');
+        img.src = `${bau.foto1}`
+
+        box.appendChild(img);
+
+        img.addEventListener("click", ()=> {
+
+            modal.style.display = 'block';
+            textPremio.innerHTML = `${bau.premio}`
+            img.style.display = 'none';
+
+        })
+
+        console.log(bau.premio)
+
+    })
+
+
+  }
+
+
+  function reiniciarGame(){
+
+    location.reload();
+  }
+
+
+  ListarBaus()
